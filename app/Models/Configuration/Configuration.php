631a105c449d0edba033b6ca3897ea377d55903c
mawei <marcelweirather@outlook.de>
2024-01-27 17:14:55 +0100

    created configuration model

---
+++ app/Models/Configuration/Configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Configuration;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Configuration extends Model
+{
+    use HasFactory;
+}
