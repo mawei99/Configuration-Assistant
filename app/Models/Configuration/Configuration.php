6d8867d3e3ec617c845b913b1e5b1fb32ef309dc
mawei <marcelweirather@outlook.de>
2024-01-27 20:31:20 +0100

    removed Property resource

--- app/Models/Configuration/Configuration.php
+++ app/Models/Configuration/Configuration.php
@@ -4,8 +4,13 @@ namespace App\Models\Configuration;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Configuration extends Model
 {
     use HasFactory;
+
+    public function configurationTemplate(): BelongsTo {
+        return $this->belongsTo(ConfigurationTemplate::class);
+    }
 }
