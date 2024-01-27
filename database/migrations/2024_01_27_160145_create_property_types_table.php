81464127bf4a82f8b9d0585bd71f316ac9f0064d
mawei <marcelweirather@outlook.de>
2024-01-27 17:16:45 +0100

    created property type model

---
+++ database/migrations/2024_01_27_160145_create_property_types_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('property_types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type');
+        });
+    }
+    public function down(): void {
+        Schema::dropIfExists('property_types');
+    }
+};
